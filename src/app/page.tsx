887aaea3af5d0a68c19d12a2cfe3c2ef687d4211
sankaran 2006 <viji.senthil2013@gmail.com>
2025-01-17 13:33:53 +0530

    comitted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "./landing/Hero";
+import Ad from "./landing/Ad";
 
 export default function Home() {
   return (
     <>
 <Hero></Hero>
+<Ad></Ad>
    </>
   );
 }
