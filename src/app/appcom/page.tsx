37ef65070fbfa1be0a40455f353de90e31a53977
sankaran 2006 <viji.senthil2013@gmail.com>
2025-01-22 01:26:12 +0530

    sankar push

--- src/app/appcom/page.tsx
+++ src/app/appcom/page.tsx
@@ -154,7 +154,8 @@ const username = searchParams?.get("username") || "Guest";
   return (
     
     <div className="flex flex-1">
-      <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full  overflow-y-auto">
+      <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700
+       bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full  overflow-y-auto"  style={{ maxHeight: "100vh" }}>
         {selectedSection === "Create" ? (
           <Create  username={username}  /> // Should render here
         ) : selectedSection === "Explore" ? (
