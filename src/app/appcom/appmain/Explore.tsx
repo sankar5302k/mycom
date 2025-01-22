a9bf92f1a4a02326c7466230c716c6609f4da0d1
sankaran 2006 <viji.senthil2013@gmail.com>
2025-01-22 22:35:30 +0530

    sankar push

--- src/app/appcom/appmain/Explore.tsx
+++ src/app/appcom/appmain/Explore.tsx
@@ -84,12 +84,15 @@ const Explore: React.FC = () => {
       {!loading && (
         <button
           onClick={loadMore}
-          className="block mx-auto mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          className="block mx-auto mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-full sm:w-auto"
         >
           Load More
         </button>
       )}
       {loading && <p className="text-center text-gray-500">Loading...</p>}
+      <br></br>
+      <br></br>
+      <br></br>
     </div>
   );
 };
