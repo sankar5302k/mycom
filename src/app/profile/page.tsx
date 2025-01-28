1360c37a9f64e3575bc7b13db538ad63cbb3f6fb
sankaran 2006 <viji.senthil2013@gmail.com>
2025-01-28 23:01:46 +0530

    sankar push

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,5 @@
-"use client";
-
 import { useSearchParams } from "next/navigation";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 
 export default function ProfileComponent() {
   const searchParams = useSearchParams();
@@ -26,14 +24,16 @@ export default function ProfileComponent() {
   }, [searchParams]);
 
   return (
-    <div className="flex flex-col items-center p-6">
-      <h1 className="text-2xl font-semibold mt-4">{userDetails.username}</h1>
-      <div className="mt-4 text-center">
-        {userDetails.country && <p>Country: {userDetails.country}</p>}
-        {userDetails.state && <p>State: {userDetails.state}</p>}
-        {userDetails.district && <p>District: {userDetails.district}</p>}
-        {userDetails.area && <p>Area: {userDetails.area}</p>}
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="flex flex-col items-center p-6">
+        <h1 className="text-2xl font-semibold mt-4">{userDetails.username}</h1>
+        <div className="mt-4 text-center">
+          {userDetails.country && <p>Country: {userDetails.country}</p>}
+          {userDetails.state && <p>State: {userDetails.state}</p>}
+          {userDetails.district && <p>District: {userDetails.district}</p>}
+          {userDetails.area && <p>Area: {userDetails.area}</p>}
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
